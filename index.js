4240299e629581b6bb6f6b7c7ff3c79fcc210e5f
Gourav Ojha <46746385+gouravojha@users.noreply.github.com>
2021-04-19 21:16:18 +0530

    Added news feature

--- index.js
+++ index.js
@@ -68,3 +68,41 @@ function getData(){
 		console.error(err);
 	});
 }    
+
+
+
+// News
+fetch("https://covid-19-news.p.rapidapi.com/v1/covid?q=covid&lang=en&country=IN&topic=news&page=5&media=True", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "7c615dcca4msh1256e61cbc40572p1f9723jsn712b498418d0",
+		"x-rapidapi-host": "covid-19-news.p.rapidapi.com"
+	}
+})
+.then(response => {
+	return (response.json());
+}).then(data =>{
+	x = data['articles']
+	console.log(data)
+	document.getElementById('c-link').href = x[0]['link'];
+	document.getElementById('c-link2').href = x[1]['link'];
+	document.getElementById('c-link3').href = x[2]['link'];
+	document.getElementById('c-link4').href = x[3]['link'];
+	document.getElementById('c-link5').href = x[4]['link'];
+	document.getElementById('c-link6').href = x[5]['link'];
+	document.getElementById('c-img').src = x[0]['media'];
+	document.getElementById('c-img2').src = x[1]['media'];
+	document.getElementById('c-img3').src = x[2]['media'];
+	document.getElementById('c-img4').src = x[3]['media'];
+	document.getElementById('c-img5').src = x[4]['media'];
+	document.getElementById('c-img6').src = x[5]['media'];
+	document.getElementById('c-title').innerHTML = x[0]['title']
+	document.getElementById('c-title2').innerHTML = x[1]['title']
+	document.getElementById('c-title3').innerHTML = x[2]['title']
+	document.getElementById('c-title4').innerHTML = x[3]['title']
+	document.getElementById('c-title5').innerHTML = x[4]['title']
+	document.getElementById('c-title6').innerHTML = x[5]['title']
+})
+.catch(err => {
+	console.error(err);
+});
